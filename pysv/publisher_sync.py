6b1274256575bda3b087b76fc907e22e909234d7
Arthur Zopellaro <arthurazs@id.uff.br>
2022-11-21 15:15:28 -0300

    Fix pub_sync not changing interface

--- pysv/publisher_sync.py
+++ pysv/publisher_sync.py
@@ -1,12 +1,13 @@
 from socket import AF_PACKET, SOCK_RAW, socket
+from sys import argv
 
 from pysv.sv import DEFAULT_PATH, generate_sv_from
 from pysv.utils import usleep
 
 
-def run() -> None:
+def run(interface: str) -> None:
     with socket(AF_PACKET, SOCK_RAW, 0xBA88) as nic:
-        nic.bind(("lo", 0))
+        nic.bind((interface, 0))
 
         for header, pdu in generate_sv_from(DEFAULT_PATH):
             usleep(250)
@@ -14,4 +15,4 @@ def run() -> None:
 
 
 if __name__ == "__main__":
-    run()
+    run(argv[1])
